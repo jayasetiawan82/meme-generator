5493320e375684cbb21f8746e44d70a4abcc271f
jayasetiawan82 <jayasetiawan28@gmail.com>
2024-03-09 15:11:02 +1030

    initialize

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../images/troll-face.png"
+export default function Header() {
+    return (
+        <header className="header">
+        <img src={logo} alt="logo"></img>
+        <h2 className="header-title">MemeGenerator</h2>
+        <h4 className="header-project">React Course - Project 3</h4>
+        </header>
+    )
+}
---
+++ src/components/Meme.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <main>
+      <form className="form">
+        <input type="text" placeholder="Top Text" className="form-input" />
+
+        <input type="text" placeholder="Bottom Text" className="form-input" />
+
+        <button>Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
+}
