53a2b2a39affbc6a078baa7a8d2e50678af247ab
jayasetiawan82 <jayasetiawan28@gmail.com>
2024-03-12 10:08:11 +1030

    upload

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,32 @@
 import React from "react";
+import memesData from "./memesData";
 
 export default function Meme() {
+    let url;
+    function getMemeImage(event) {
+        event.preventDefault()
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+
+    }
+
   return (
     <main>
       <form className="form">
+        <div>
+        <label>Top Text</label>
         <input type="text" placeholder="Top Text" className="form-input" />
-
+        </div>
+        
+        <div>
+            <label>Bottom Text</label>
         <input type="text" placeholder="Bottom Text" className="form-input" />
+        </div>  
+        
 
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getMemeImage}>Get a new meme image 🖼</button>
       </form>
     </main>
   );
